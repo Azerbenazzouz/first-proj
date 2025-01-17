2bdd9fff1cca2947a3949cb7547b2ae0922c3676
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-17 10:45:12 +0100

    Ajout de la page produit dynamique avec récupération de l'identifiant du produit

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Product Page {{ id }} </h1>
+        <p>Product page content</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
