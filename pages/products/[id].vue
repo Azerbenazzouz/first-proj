a65db9d0d9a19fbbd1cda720ef3d3529ec84c7d9
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-17 16:12:52 +0100

    Ajout d'une gestion des erreurs 404 pour les produits non trouvés et création d'une page d'erreur personnalisée

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -7,6 +7,9 @@
 <script setup>
 const { id } = useRoute().params
 const { data: product } = await useFetch(`https://fakestoreapi.com/products/${id}`)
+if(!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+}
 </script>
 
 <style lang="scss" scoped>
