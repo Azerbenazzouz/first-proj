46c589bf4952048d0283dae29e837e6f685466c2
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-17 10:56:54 +0100

    Ajout d'un en-tête de navigation sur la page d'accueil

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,22 @@
 </script>
 
 <template>
+    <header>
+        <nav>
+            <NuxtLink to="/">Nuxt Azer</NuxtLink>
+            <ul>
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/products">Products</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
     <div>
         <h1>Home Page</h1>
         <p>Home page content</p>
