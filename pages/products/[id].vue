09ad8b27c6d5ab6b490f951bc4692aea17efd4ba
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-17 14:52:13 +0100

    Amélioration de la page produit et de la liste des produits avec un nouveau style et récupération des données depuis l'API

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <h1>Product Page {{ id }} </h1>
-        <p>Product page content</p>
+        <div class="bg-white p-5 shadow rounded-lg">
+            <h1 class="text-lg font-semibold">{{ product.title }}</h1>
+            <p class="text-gray-500">{{ product.description }}</p>
+            <p class="text-blue-500 font-semibold">${{ product.price }}</p>
+        </div>
     </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params
+const { data: product } = await useFetch(`https://fakestoreapi.com/products/${id}`)
 </script>
 
 <style lang="scss" scoped>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <h1>Products Page</h1>
-        <p>Products page content</p>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="product in products" :key="product.id" class="bg-white p-5 shadow rounded-lg">
+                <NuxtLink :to="`/products/${product.id}`" class="text-lg font-semibold">{{ product.title }}</NuxtLink>
+                <p class="text-gray-500">{{ product.description }}</p>
+                <p class="text-blue-500 font-semibold">${{ product.price }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,6 +15,10 @@
     definePageMeta({
         layout: 'products',
     })
+
+    // Fetch products
+    const { data : products } =await useFetch('https:fakestoreapi.com/products');
+    console.log(products);
 </script>
 
 <style scoped>
